f3903eb99ba68d62e750b9c8af380662269356d5
HawkQing1 <hawk_qing@epam.com>
2026-07-23 14:07:12 +0800

    fix: [EL-5939] System crash occurs when opening a chat-generated .mdx file in Preview mode (#625)

--- src/[fsd]/features/artifacts/lib/helpers/previewMdx.helpers.js
+++ src/[fsd]/features/artifacts/lib/helpers/previewMdx.helpers.js
@@ -6,13 +6,15 @@ export const stripMdxMetadata = source => {
 
 export const evaluateMdx = async source => {
   const processed = stripMdxMetadata(source);
-  const [{ evaluate }, runtime, { default: remarkGfm }] = await Promise.all([
+  const [{ evaluate }, runtime, { default: remarkGfm }, { useMDXComponents }] = await Promise.all([
     import('@mdx-js/mdx'),
     import('react/jsx-runtime'),
     import('remark-gfm'),
+    import('@mdx-js/react'),
   ]);
   const { default: MDXContent } = await evaluate(processed, {
     ...runtime,
+    useMDXComponents,
     remarkPlugins: [remarkGfm],
     development: false,
   });
