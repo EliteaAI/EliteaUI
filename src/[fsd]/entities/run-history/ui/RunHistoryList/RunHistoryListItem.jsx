a5a9d0f51ff7962b351ea2abff5ebc52a9379200
Alexander Bychinskiy <bermudas.alexander@gmail.com>
2026-08-02 04:03:35 +0300

    test: [EL-0000] add data-testid + data-selected for Run History list item (ELITEA-1877)

--- src/[fsd]/entities/run-history/ui/RunHistoryList/RunHistoryListItem.jsx
+++ src/[fsd]/entities/run-history/ui/RunHistoryList/RunHistoryListItem.jsx
@@ -139,6 +139,8 @@ const RunHistoryListItem = memo(props => {
   return (
     <>
       <Box
+        data-testid="run-history-list-item"
+        data-selected={selectedItem === item.id}
         sx={[styles.listItem, selectedItem === item.id && styles.selected]}
         onClick={() => onItemSelect(item.id)}
       >
