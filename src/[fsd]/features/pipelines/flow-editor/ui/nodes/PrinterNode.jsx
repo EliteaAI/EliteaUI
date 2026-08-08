b65756afc6097872f9cef7e8a080b83724f5e89b
Alexander Bychinskiy <bermudas.alexander@gmail.com>
2026-08-08 12:23:46 +0300

    test: [EL-0000] add data-testid for Printer node target/source connection handles (ELITEA-2039)

--- src/[fsd]/features/pipelines/flow-editor/ui/nodes/PrinterNode.jsx
+++ src/[fsd]/features/pipelines/flow-editor/ui/nodes/PrinterNode.jsx
@@ -77,6 +77,7 @@ const PrinterNode = memo(props => {
               isConnectable={!isRunningPipeline && !disabled}
               isRunningPipeline={isRunningPipeline}
               isPerforming={data?.isPerforming}
+              testId="pipeline-printer-node-target-handle"
             />
             <FlowEditorNodes.CustomHandle
               type="source"
@@ -84,6 +85,7 @@ const PrinterNode = memo(props => {
               isConnectable={isSourceConnectable && !isRunningPipeline && !disabled}
               isRunningPipeline={isRunningPipeline}
               isPerforming={data?.isPerforming}
+              testId="pipeline-printer-node-source-handle"
             />
           </>
         );
